6c6c9bd76d058ef272045a3c8238369f82bdc973
ᴹᴿ᭄ Fʀᴀɴᴋ ᴏғᴄ 々 <darrelmucheri@gmail.com>
2024-05-03 18:24:07 +0200

    Initialize Fixes-by-TJ

---
+++ replit.nix
@@ -0,0 +1,18 @@
+{ pkgs }: {
+    deps = [
+        pkgs.ffmpeg
+        pkgs.git
+        pkgs.nodejs-18_x
+        pkgs.nodePackages.typescript
+        pkgs.libwebp
+        pkgs.python
+        pkgs.libuuid
+        pkgs.imagemagick
+        pkgs.pm2
+        pkgs.wget
+        pkgs.yarn
+    ];
+  env ={
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];
+  };
+}
